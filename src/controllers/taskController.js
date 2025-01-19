6f93be239c8c9348ea11a233c0f9a270e8bd919d
bauti.iglesias@gmail.com <bauti.iglesias@gmail.com>
2025-01-19 19:30:32 -0300

    database connection

--- src/controllers/taskController.js
+++ src/controllers/taskController.js
@@ -1,12 +1,56 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.createTask = exports.getTasks = void 0;
-const getTasks = (req, res) => {
-    res.json({ message: 'List of tasks' });
-};
+exports.deleteTask = exports.createTask = exports.getTasks = void 0;
+const Task_1 = __importDefault(require("../models/Task"));
+// GET
+const getTasks = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const tasks = yield Task_1.default.find();
+        res.status(200).json(tasks);
+    }
+    catch (error) {
+        res.status(500).json({ message: 'Error fetching tasks', error });
+    }
+});
 exports.getTasks = getTasks;
-const createTask = (req, res) => {
-    const { title } = req.body;
-    res.json({ message: `Task '${title}' created!` });
-};
+// POST
+const createTask = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const { title, description } = req.body;
+        const newTask = new Task_1.default({ title, description });
+        const savedTask = yield newTask.save();
+        res.status(201).json(savedTask);
+    }
+    catch (error) {
+        res.status(500).json({ message: 'Error creating task', error });
+    }
+});
 exports.createTask = createTask;
+// DELETE
+const deleteTask = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const { id } = req.params;
+        const deletedTask = yield Task_1.default.findByIdAndDelete(id);
+        if (!deletedTask) {
+            res.status(404).json({ message: 'Task not found' });
+            return;
+        }
+        res.status(200).json({ message: 'Task deleted successfully', task: deletedTask });
+    }
+    catch (error) {
+        res.status(500).json({ message: 'Error deleting task', error });
+    }
+});
+exports.deleteTask = deleteTask;
